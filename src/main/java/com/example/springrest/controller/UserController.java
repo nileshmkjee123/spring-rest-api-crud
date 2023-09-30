ba873fb7a726398373fea88c2b3263ded4515cad
nilesh65 <nileshmukherjee65@gmail.com>
2023-09-30 23:34:46 +0530

    Spring REST API based crud app

---
+++ src/main/java/com/example/springrest/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.springrest.controller;
+
+import com.example.springrest.entity.User;
+import com.example.springrest.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("api/users")
+public class UserController {
+
+    private UserService userService;
+
+    // create user REST API
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user)
+    {
+        User savedUser = userService.createUser(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+    // get user by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId)
+    {
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user,HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users,HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id")Long id,
+                                           @RequestBody User user)
+    {   user.setId(id);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser,HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable("id")Long id)
+    {
+        userService.deleteUser(id);
+        return new ResponseEntity<>("Deleted user: "+id,HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/springrest/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.springrest.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(nullable = false,unique = true)
+    private String email;
+}
